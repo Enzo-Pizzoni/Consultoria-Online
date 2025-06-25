e05aed40d71b6fbc553858a04624793deef13d0f
Enzo-Pizzoni <enzopsette@gmail.com>
2025-06-25 12:20:08 -0300

    refactor: ConnectionFactory pode ser usado em outros módulos

--- ConnectionFactory/ConnectionFactory.py
+++ ConnectionFactory/ConnectionFactory.py
@@ -1,17 +1,22 @@
 import mysql.connector
+from mysql.connector import Error
 import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-conexao = mysql.connector.connect(
-    host= os.getenv("MYSQL_HOST"),
-    user= os.getenv("MYSQL_USER"),
-    password= os.getenv("MYSQL_PASSWORD"),
-    database= os.getenv("MYSQL_DATABASE")
-)
-
-cursor = conexao.cursor()
-
-cursor.close()
-conexao.close() 
+def criar_conexao():
+    try:
+        conexao = mysql.connector.connect(
+            host= os.getenv("MYSQL_HOST"),
+            user= os.getenv("MYSQL_USER"),
+            password= os.getenv("MYSQL_PASSWORD"),
+            database= os.getenv("MYSQL_DATABASE")
+        )
+        if conexao.is_connected():
+            print("Conexão bem-sucedida!")
+            return conexao
+    except Error as erro:
+        print("Erro ao conectar ao banco de dados:")
+        print(erro)
+        return None
